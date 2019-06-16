410678d00919f4a13ff80933d536c484309f9ced
Vlad Traistaru <vladtherocker@gmail.com>
2019-06-16 20:04:08 +0300

    Minor UI changes
    Blinking score text, random quote of the day text, moved buttons around, trying my best to scale for 16:9 and others

---
+++ Assets/Scripts/Utils/BlinkingText.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+using System.Collections;
+
+public class BlinkingText : MonoBehaviour
+{
+    public TextMeshProUGUI myText;
+    public bool isFlashing;
+    public float flashTime = 0.5f;
+
+    void Start()
+    {
+        StartCoroutine(BlinkText());
+    }
+
+    IEnumerator BlinkText()
+    {
+        while (isFlashing)
+        {
+            myText.alpha = 0.0f;
+            yield return new WaitForSeconds(flashTime);
+            myText.alpha = 1.0f;
+            yield return new WaitForSeconds(flashTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/RandomLineFromFile.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class RandomLineFromFile : MonoBehaviour
+{
+    public TextMeshProUGUI myText;
+    public string frontText;
+    public string endText;
+    public TextAsset file;
+    void Start()
+    {
+        myText.text = "";
+        string[] lines = file.ToString().Split('\n');
+        if (!string.IsNullOrEmpty(frontText))
+        {
+            myText.text += frontText;
+        }
+        myText.text += lines[Random.Range(0, max: lines.Length)];
+        if (!string.IsNullOrEmpty(endText))
+        {
+            myText.text += endText;
+        }
+    }
+}
