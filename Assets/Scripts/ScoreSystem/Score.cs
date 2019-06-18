3299b815ede84d0c243764a201c092b633658533
Vlad Traistaru <vladtherocker@gmail.com>
2019-06-18 20:26:39 +0300

    Gameplay systems updated
    Score system added, collisions are broken

---
+++ Assets/Scripts/ScoreSystem/Score.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+
+    public TextMeshProUGUI myScore;
+    public bool addToScore = true;
+    public int startScore = 0;
+    public int rateOfScore = 10;
+    public float secondsBetweenAdds = 0.2f;
+    public int multiplier = 1;
+    public int howManyLeadingZeros = 6;
+
+    private bool startedOnce = false;
+
+    void Update()
+    {
+        if (!startedOnce)
+        {
+            startedOnce = true;
+            StartCoroutine(AddScore());
+        }
+    }
+
+    IEnumerator AddScore()
+    {
+        while (addToScore)
+        {
+            myScore.text = "";
+            startScore += rateOfScore * multiplier;
+            for(int i = startScore.ToString().Length; i < howManyLeadingZeros; i++)
+            {
+                myScore.text += '0';
+            }
+            myScore.text += startScore.ToString();
+            yield return new WaitForSeconds(secondsBetweenAdds);
+        }
+        startedOnce = false;
+    }
+}
